4f279d8418a32207aa4d25239c68396e28c0d940
Tarik Cekirge <tcekirge@gmail.com>
2022-12-22 20:14:51 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  password = "";
+  length:Number = 0;
+  includeLetters:boolean= false;
+  includeNumbers:boolean= false;
+  includeSymbols:boolean= false;
+  modifyLength(value:string){
+    console.log(value);
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+  modifyLetters(){
+    this.includeLetters =!this.includeLetters;
+  };
+  modifyNumbers(){
+    this.includeNumbers =!this.includeNumbers;
+  };
+  modifySymbols(){
+    this.includeSymbols =!this.includeSymbols;
+  };
+
+  generateBtn(){
+
+   const numbers='1234567890';
+   const letters='abcdefghijklmnopqrstuvwyz';
+   const symbols='!@#$%^&*()';
+   let validChars = "";
+
+   if(this.includeLetters){
+    validChars+=letters;
+   };
+   if(this.includeNumbers){
+    validChars+=numbers;
+   };
+   if(this.includeSymbols){
+    validChars+=symbols;
+   };
+
+   let generatedPassword ="";
+
+   for(let i = 0; i<this.length; i++){
+    const index = Math.floor(Math.random()*(validChars.length));
+    generatedPassword += validChars[index];
+   }
+
+   this.password = generatedPassword;
+
+
+  };
 }
